19b801e21a6b2139e472e8edad134b6a04f5fb7b
Audy Wallace Siegle <audyblesound@gmail.com>
2025-02-20 22:25:47 +0800

    "add profile image url field to user profile"

--- models/user_profile.go
+++ models/user_profile.go
@@ -7,11 +7,12 @@ import (
 )
 
 type UserProfile struct {
-	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"` // Creates a version 4 UUID
-	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null;unique"`
-	User      User      `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE, OnDelete:CASCADE;"`
-	BirthDate time.Time `json:"birth_date" gorm:"not null; check:birth_date >= '1900-01-01' AND birth_date <= now()"` // Birthdates must be valid
-	Bio       string    `json:"bio" gorm:"type:varchar(500);check:length(bio) >= 10;not null"`                        // Bio length must be between 10 to 500
-	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
-	UpdatedAt time.Time `json:"updated_at" gorm:"autoCreateTime"`
+	ID              uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"` // Creates a version 4 UUID
+	UserID          uuid.UUID `json:"user_id" gorm:"type:uuid;not null;unique"`
+	User            User      `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE, OnDelete:CASCADE;"`
+	BirthDate       time.Time `json:"birth_date" gorm:"not null; check:birth_date >= '1900-01-01' AND birth_date <= now()"` // Birthdates must be valid
+	Bio             string    `json:"bio" gorm:"type:varchar(500);check:length(bio) >= 10;not null"`                        // Bio length must be between 10 to 500
+	ProfileImageURL string    `json:"profile_image_url" gorm:"not null"`
+	CreatedAt       time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt       time.Time `json:"updated_at" gorm:"autoCreateTime"`
 }
