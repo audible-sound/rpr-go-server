e542f4531c5693c283285c0ee9299d7cb717b720
Audy Wallace Siegle <audyblesound@gmail.com>
2025-02-20 22:05:21 +0800

    add user and user profile model

---
+++ models/user.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type User struct {
+	ID        uuid.UUID      `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"` // Creates a version 4 UUID
+	Hash      string         `json:"-" gorm:"not null"`                                         // Hide from JSON Response
+	Email     string         `json:"email" gorm:"unique;not null"`
+	Username  string         `json:"username" gorm:"unique; not null"`
+	CreatedAt time.Time      `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updated_at" gorm:"autoCreateTime"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"` // Enable soft delete, user record will be excluded when fetching data
+}
---
+++ models/user_profile.go
@@ -0,0 +1,17 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type UserProfile struct {
+	ID        uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey"` // Creates a version 4 UUID
+	UserID    uuid.UUID `json:"user_id" gorm:"type:uuid;not null;unique"`
+	User      User      `json:"user" gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE, OnDelete:CASCADE;"`
+	BirthDate time.Time `json:"birth_date" gorm:"not null; check:birth_date >= '1900-01-01' AND birth_date <= now()"` // Birthdates must be valid
+	Bio       string    `json:"bio" gorm:"type:varchar(500);check:length(bio) >= 10;not null"`                        // Bio length must be between 10 to 500
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoCreateTime"`
+}
